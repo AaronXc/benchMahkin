06a6718d9b7557a1c07a98c56c640fc39e3a8e3b
AaronXc <aaronwilcox2000@gmail.com>
2020-12-02 13:57:53 -0400

    working version for reads and writes on separate graphs

--- GraphPerformance.py
+++ GraphPerformance.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import  numpy as np
 import time
 
+
+
 directoryNames=[
                 "read_noAlt", "read_altPorts", "read_altCards", "read_altPortsAndCards",
                 "randwrite_noAlt", "randwrite_altPorts", "randwrite_altCards", "randwrite_altPortsAndCards",
@@ -16,6 +18,8 @@ directoryNames=[
                 ]
 globalSections=["read", "randwrite", "randrw"]
 driveOrders = ["noAlt", "altPorts", "altCards", "altPortsAndCards"]
+
+ioType = ["/reads", "/writes"]
 
 hardware=open("./BenchMKresults/hardware", "r+")
 path=hardware.readline()  
@@ -39,83 +43,65 @@ json.close()
 for section in globalSections:
     for driveOrder in driveOrders:
         directoryNameIndex = driveOrders.index(driveOrder)+len(driveOrders)*globalSections.index(section)
+        
+        testName="BenchMKresults_"+directoryNames[directoryNameIndex]
+        os.mkdir("./AutoSavedResults/"+path+"/"+testName)
+        
+        for io in ioType:
+            if os.path.exists("BenchMKresults/"+testName+io):
+                graph_Coordinates_to_be=open("BenchMKresults/"+testName+io, "r") 
+                results = graph_Coordinates_to_be.readlines()
+                graph_Coordinates_to_be.close()   
+                
+                data=open("./AutoSavedResults/"+path+"/"+testName+"/"+"graph_Coordinates", "w+")
+                for line in results:
+                    data.write(line)
+                data.close()
+                
+                y_axis = []
+                x_axis = []
+                ALLtheData=[]
+                IOPS_y_axis=[] 
+                BW_y_axis=[]   
+                
+                for line in results:
+                    throughput = re.search("(\d+)\s+(\d+\.*\d*)\s+(\d+\.*\d*)", line)
+                    if throughput != None:
+                        print(throughput)
+                        if throughput.group(1) not in x_axis:
+                            x_axis.append(throughput.group(1))
+                            ALLtheData.append((throughput.group(1), throughput.group(2), throughput.group(3))) 
+    
+                print(ALLtheData)
+                print(x_axis)
+                for x_val in x_axis:
+                    for dataPoint in ALLtheData:
+                        if dataPoint[0] == x_val:
+                            BW=float(dataPoint[2])
+                            IOPS=float(dataPoint[1])
+                            IOPS_y_axis.append(IOPS)
+                            BW_y_axis.append(BW)     
+                
+                skip=2
+                locations=(np.arange(1,len(x_axis), skip))
+                labels=[]
+                for loc in locations:
+                    labels.append(str(loc))   
+                fig, (ax1, ax2) = plt.subplots(2)
+                fig.suptitle("Stornado Performance")
+                ax1.plot(x_axis, IOPS_y_axis, "tab:red")
+                ax2.plot(x_axis, BW_y_axis)
+                plt.xlabel("number of drives used in I/O operations")
+                ax1.set(ylabel="IOPS (operations/s)")
+                ax2.set(ylabel="throughput (MiB/s)")
+                ax1.set_xticks(ticks=locations)
+                ax2.set_xticks(ticks=locations)
+                ax1.grid(True)
+                ax2.grid(True)
+                #plt.show()
+                plt.savefig("./AutoSavedResults/"+path+"/"+testName+io+"_graphs.png")
    
-        graph_Coordinates=open("BenchMKresults/"+"BenchMKresults_"+directoryNames[directoryNameIndex], "r")
-        filename=graph_Coordinates.readline().strip()
-        results = graph_Coordinates.readlines()
-        graph_Coordinates.close()
 
-        os.mkdir("./AutoSavedResults/"+path+"/"+filename)
-        data=open("./AutoSavedResults/"+path+"/"+filename+"/"+"graph_Coordinates", "w+")
-        for line in results:
-            data.write(line)
-        data.close()
-        
-        y_axis = []
-        x_axis = []
-        ALLtheData=[]
-        IOPS_y_axis=[] 
-        BW_y_axis=[]   
-        
-        for line in results:
-            throughput = re.search("(\d+)\s+(\d+\.*\d*)\s+(\d+\.*\d*)", line)
-            if throughput != None:
-                print(throughput)
-                if throughput.group(1) not in x_axis:
-                    x_axis.append(throughput.group(1))
-                ALLtheData.append((throughput.group(1), throughput.group(2), throughput.group(3)))   
-        print("all the data: ", ALLtheData)  
-        for dataPoint in ALLtheData:
-            if dataPoint != "used":
-                IOPSdata = float(dataPoint[1])
-                BWdata = float(dataPoint[2])
-                temp = ["blank"]
-                for j in range(ALLtheData.index(dataPoint)+1, len(ALLtheData)):
-                    if ALLtheData[j] != None and ALLtheData[j][0] == dataPoint[0]:
-                        temp.append("blank")
-                        IOPSdata+=float(ALLtheData[j][1])
-                        BWdata+=float(ALLtheData[j][2])
-                        ALLtheData[j]="used"
-                BWdata=BWdata/len(temp)
-                IOPSdata=IOPSdata/len(temp)
-                print("IOPS: ", IOPSdata)
-                print("BW: ", BWdata)        
-                index=ALLtheData.index(dataPoint)
-                x_val = dataPoint[0]
-                ALLtheData.remove(dataPoint) 
-                ALLtheData.insert(index, (x_val, IOPSdata, BWdata))
-        
-        print(ALLtheData)
-        print(x_axis)
-        averages = open("./AutoSavedResults/"+path+"/"+filename+"/"+"average values.txt", "a+")
-        for x_val in x_axis:
-            for dataPoint in ALLtheData:
-                if dataPoint[0] == x_val:
-                    IOPS_y_axis.append(dataPoint[1])
-                    BW_y_axis.append(dataPoint[2])
-                    BW=str(dataPoint[2])
-                    IOPS=str(dataPoint[1])
-                    averages.write(dataPoint[0]+","+IOPS+","+BW+"\n")
-        averages.close()        
-        
-        skip=2
-        locations=(np.arange(1,len(x_axis), skip))
-        labels=[]
-        for loc in locations:
-            labels.append(str(loc))   
-        fig, (ax1, ax2) = plt.subplots(2)
-        fig.suptitle("Stornado Performance")
-        ax1.plot(x_axis, IOPS_y_axis, "tab:red")
-        ax2.plot(x_axis, BW_y_axis)
-        plt.xlabel("number of drives used in I/O operations")
-        ax1.set(ylabel="IOPS (operations/s)")
-        ax2.set(ylabel="throughput (MiB/s)")
-        ax1.set_xticks(ticks=locations)
-        ax2.set_xticks(ticks=locations)
-        ax1.grid(True)
-        ax2.grid(True)
-        #plt.show()
-        plt.savefig("./AutoSavedResults/"+path+"/"+filename+"/graphs.png")
 
 
 
